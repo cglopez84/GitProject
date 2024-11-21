bbc9e3665b2d56f57cf3d93e4c4c6a1aedd13e81
CarlosG <cglopez@minsait.com>
2024-11-21 10:37:06 +0100

    commit hacer un stash

--- Prueba/src/Segundo/OtraClase.java
+++ Prueba/src/Segundo/OtraClase.java
@@ -4,7 +4,7 @@ class OtraClase{
 	
 	String txt = "texto de prueba nuevo para conflicto";
 
-	String txt2 = "modificacion adicional";
+	String txt2 = "modificacion adicional  ";
 	
 	String txt3 ="   clase jueves 21";
 }
